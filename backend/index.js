075881cc60fc0b0563ff30a47e09990aafe90071
jaiswaldeepali27 <jaiswaldeepali110@gmail.com>
2026-08-12 17:01:24 +0530

    route setup

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,17 @@
 import express from "express";
+import dotenv from "dotenv";
+
+import { connectDB } from "./db/connectDB.js";
+import authRoutes from "./routes/auth.route.js";
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-    res.send("Hello 12");
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+    connectDB();
+    console.log("Server is running", PORT);
 });
-
-app.listen(3000, () => {
-    console.log("Server is running");
-});
-
-m2y1U4BwwmvjUWpI;
