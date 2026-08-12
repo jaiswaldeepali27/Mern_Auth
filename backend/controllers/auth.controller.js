ce0f215c7fcd2b6852c519098d5e55fa6dee52bc
jaiswaldeepali27 <jaiswaldeepali110@gmail.com>
2026-08-12 18:52:14 +0530

    update

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,39 @@
+import { User } from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import { generateVerificationCode } from "../utils/generateVerificationCode.js";
+
 export const signup = async (req, res) => {
-    res.send("signup route");
-}
+    const {email, password, name} = req.body;
+
+    try {
+        if(!email || !password || !name) {
+            throw new Error("All fields are required");
+        }
+
+        const userAlreadyExists = await User.findOne({email});
+        if (userAlreadyExists) {
+            return res.status(400).json({success:false, message: "user already exists"});
+        }
+
+        const hashedPassword = await bcryptjs.hash(password, 10);
+        const verificationCode = generateVerificationCode();
+
+        const user = new User({
+            email,
+            password: hashedPassword,
+            name,
+            verificationToken: verificationCode,
+            verifiationTokenExpiresAt: Date.now() + 24 * 60 * 60 * 1000 // 24 hrs
+        })
+
+        await user.save();
+
+        //jwt
+        generateTokenAndSetCookie(res, user._id);
+    } catch (error) {
+        res.status(400).json({success:false, message: error.message});
+    }
+};
 
 export const login = async (req, res) => {
     res.send("login route");
--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json()); // allows us to parse incoming requests: req.body
+
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
