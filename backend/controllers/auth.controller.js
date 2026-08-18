f9ee8dab44439672cb1d5828ffd2a4d8da50e061
jaiswaldeepali27 <jaiswaldeepali110@gmail.com>
2026-08-18 18:56:29 +0530

    update

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -66,15 +66,62 @@ export const verifyEmail = async (req, res) => {
         await user.save();
 
         await sendWelcomeEmail( user.email, user.name);
-    } catch(error){
 
+        res.status(200).json({
+            success: true,
+            message: "Email verified successfully",
+            user: {
+                ...user._doc,
+                password: undefined,
+            },
+        });
+    } catch(error){
+        console.log("error in verifyemail", error);
+        res.status(500).json({success:false, message: "Server error"});
     }
 }
 
 export const login = async (req, res) => {
-    res.send("login route");
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({email});
+        if(!user) {
+            return res.status(400).json({ success: false, message: "invalid credentials"});
+        }
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if(!isPasswordValid) {
+            return res.status(400).json({success: false, message: "Invalid Credentails"});
+        }
+
+        generateTokenAndSetCookie(res, user._id);
+
+        user.lastLogin = new Date();
+        await user.save();
+
+        res.status(200).json({
+            success: true,
+            message: "Logged in successfully",
+            user: {
+                ...user._doc,
+                password: undefined,
+            },
+        });
+    } catch (error) {
+        console.log("Error in login", error);
+        res.status(400).json({ success: false, message: error.message});       
+    }
+};
+
+export const forgotPassword = async (req, res) => {
+    const {email} = req.body;
+    try {
+        const user = await User.findOne({ email });
+    } catch (error) {
+        
+    }
 }
 
 export const logout = async (req, res) => {
-    res.send("logout route");
-}
+    res.clearCookie("token");
+    res.status(200).json({success: true, message: "Logged out successfully"});
+};
